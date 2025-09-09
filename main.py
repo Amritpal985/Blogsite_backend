066242e422033f0bc7df44e89cd104acb441b248
Amritpal985 <as0706248@gmail.com>
2025-09-10 00:11:14 +0530

    added cors middleware

--- main.py
+++ main.py
@@ -2,9 +2,17 @@ from fastapi import FastAPI
 import models
 from database import engine
 from routers import users,posts
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 models.Base.metadata.create_all(bind=engine)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(users.router)
 app.include_router(posts.router)
